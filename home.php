023a40d467d4927a73ccfb08c88d3d8d415601b8
Kutchai Jovi <kutchaijovi@gmail.com>
2019-04-23 21:38:07 +0800

    UI
    not yet polished.

--- home.php
+++ home.php
@@ -1,68 +1,102 @@
 <?php
 session_start();
+include 'func.php';
+// $name = name($username);
 ?>
 <!doctype html>
 <html>
- <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="description" content="$1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <?php
-    include_once 'conn.php';
-    ?>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>CMYK TO-DO LIST</title>
 </head>
 <body>
+<div class="container is-widescreen">
+    <div class="columns is-mobile is-centered">
+        <div class="column"></div>
+        <div class="column">
+            <div class="has-text-centered">
+                <h1 class="title">Hi, ! </h1><br>
+            </div>
+    <?php
 
-<?php
-    if (!isset($_SESSION['userid'])){
-        header('location:login.php');
-        }
+    include 'conn.php';            
+        
         $id = $_SESSION['a'];
 
-$sql = "SELECT task,date,id FROM planner WHERE accid = '$id'";
-$result = $conn->query($sql);
+        $sql = "SELECT task, duedate ,id FROM planner WHERE accid = '$id'";
+        $result = $conn->query($sql); 
 
-if ($result->num_rows > 0) {
-    echo "
-    <table>
-      <tr>
-        <th>Task</th>
-        <th>Date</th>
-        <th>Edit</th>
-        <th>Delete</th>
-      </tr>";
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<tr>
-                  <td>".$row["task"]."</td>
-                  <td>".$row["date"]."</td>
-                  <td><a href='editthis.php?id=". $row["id"] . "'><button class='btn btn-danger'>Edit</button></a></td>
-                  <td><a href='delete.php?id=". $row["id"] . "'><button class='btn btn-danger'>Done</button></a></td>
-              </tr>";
-    }
-    echo "</table>";
-} else {
-    echo "0 results";
-}
-$conn->close();
-?>
+        if ($result->num_rows > 0) : ?>
 
+            <table class="table is-hoverable is-centered is-dark font1">
+            <tbody class="has-text-centered">
+            <tr>
+                <th class="is-warning">Task</th>
+                <th class="is-warning">Date</th>
+                <th class="is-warning"></th>
+                <th class="is-warning"></th>
+            </tr>
 
-    <form method="post" action="insertTask.php">
+            <?php while($row = $result->fetch_assoc()) : ?>
+                    <tr>
+                        <td class="is-info"><b> <?php echo $row['task']; ?> </b></td>
+                        <td class="is-primary"><b> <?php echo $row['duedate']; ?> </b></td>
+                        <td> 
+                            <a href='editthis.php?id=<?php echo $row['id']; ?>'>
+                            <b>EDIT</b></button></a>
+                        </td>
+                        <td> 
+                            <a href='delete.php?id=<?php echo $row['id']; ?>'>
+                            <b>DONE</b></button></a>
+                        </td>
+                    </tr>
+            <?php endwhile; ?>
+            </tbody>
+            </table>
+    <?php else : ?>
+        <h1 class="is-white font1"><b>No task as of the moment.</b></h1><br>
+    <?php endif; 
 
-    <label id="first"> Task:</label><br/>
-    <input type="text" name="task"><br/>
+    $conn->close(); ?>
+        
+        <section id="home" class="hero is-warning is-centered">
+            <div class="hero-body">
+                <form method="post" action="insertTask.php">
+                    <div class="columns">
+                        <div class="column">
+                            <label class="font1"><b>NEW TASK</b></label>
+                            <div class="control">
+                                <input class="input is-medium" style="width:15em;" type="text" name="task" placeholder="to-do.." required>
+                            </div>
+                        </div>
 
-    <label id="second"> Date:</label><br/>
-    <input type="date" name="date"><br/>
+                        <div class="column">
+                            <label class="font1"><b>DEADLINE</b></label>
+                            <div class="control">
+                                <input class="input is-medium" style="width:15em;" type="date" name="date" placeholder="dd/mm/yyy" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="control">
+                        <button class="button is-danger font1"type="submit" name="insert"><b>ADD</b></button>
+                    </div>
+                </form>
+            </div>
+        </section><br>
 
-    <button type="submit" name="insert">Create Task</button>
+        <div class="control has-text-centered">
+            <a class="signUp font1" href="logout.php"><b>LOGOUT</b></a>
+        </div>
 
-    </form>
-    <a href="logout.php"><button>Logout</button></a>
+        </div>
+        <div class="column"></div>
+    </div>
+</div>
 
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html>
  <head>
@@ -6,38 +5,74 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="description" content="$1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <?php
-    include_once 'conn.php';
-    ?>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>CMYK TO-DO LIST</title>
+    <?php include_once 'conn.php'; ?>
 </head>
 <body>
-     
+<div class="container is-widescreen">
+<div class="columns is-mobile is-centered">
+    <div class="column"></div>
+    <div class="column">
+        <div id="cmyk" class="has-text-centered">
+            <h1 id="regCmyk" class="title" style="margin-bottom:0;">CMYK</h1>
+                <u>C</u>reate <u>MY</u> tas<u>K</u>
+        </div>
+        <section id="reg" class="hero is-dark is-centered">
+            <div class="hero-body field" style="padding: 2rem 1.5rem;">
+                <form method="post" style="padding-left: 30px;"> 
 
-    <form method="post" action="signupConn.php"> 
+                    <label class="font1"><b>USERNAME</b></label>
+                    <div class="control">
+                        <input class="input is-primary" style="width:18em;" type="text" name="username" placeholder="" required>
+                    </div><br>
+                    <label class="font1"><b>PASSWORD</b></label>
+                    <div class="control">
+                        <input class="input is-primary" style="width:18em;" type="text" name="password" placeholder="" required>
+                    </div><br>
+                    <label class="font1"><b>CONFIRM PASSWORD</b></label>
+                    <div class="control">
+                        <input class="input is-primary" style="width:18em;" type="text" name="confpassword" placeholder="" required>
+                    </div><br>
+                    <div class="control">
+                        <button class="button is-warning font1" type="submit" name="save"><b>SIGN UP</b></button>
+                    </div>
 
-    <label id="first"> Username:</label><br/>
-    <input type="text" name="username"><br/>
+                    <?php
 
-    <label id="second"> Password:</label><br/>
-    <input type="text" name="password"><br/>
+                        include 'conn.php';
+                        include 'func.php';
 
-    <label id="third"> Confirm Password:</label><br/>
-    <input type="text" name="confpassword"><br/>
+                        if(isset($_POST['save'])){
 
-    <label id="fourth"> First name:</label><br/>
-    <input type="text" name="fname"><br/>
+                            $user = $_POST["username"];
+                            $pass = $_POST["password"];
+                            $confpass = $_POST["confpassword"];
 
-    <label id="fifth"> Last name:</label><br/>
-    <input type="text" name="lname"><br/>
+                            $isValidReg = register($user,$pass,$confpass);
 
-    <label id="sixth"> Gender:</label><br/>
-    <input type="text" name="gender"><br/>
-    
+                            if($isValidReg == true){ ?>
+                                <br> Registration done! <br>
+                                <a href="index.php"><b>Start now!</b></a>
+                            <?php } else{
+                                echo '<script language="javascript">';
+                                echo 'alert("Please make sure both passwords are the same!")';
+                                echo '</script>';
+                            }    
+                        }
+                    ?>
 
-    <button type="submit" name="save">save</button>
-    </form>
-
+                </form>
+            </div>
+        </section>
+            <div class="control has-text-centered">
+                <p class="help is-white">Already registered?</p>
+                <a class="signUp font1" href="index.php"><b>SIGN IN</b></a>
+            </div>
+    </div>
+    <div class="column"></div>
+</div>
+</div>
 </body>
 </html>
