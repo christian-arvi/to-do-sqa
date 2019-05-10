d555cb6a0dfc8027ad5adf40f23fef56799c7641
Kutchai Jovi <kutchaijovi@gmail.com>
2019-05-11 00:11:09 +0800

    fixed udpate with recent UI

--- home.php
+++ home.php
@@ -6,13 +6,15 @@ include 'func.php';
 <!doctype html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="description" content="$1">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
-  <link rel="stylesheet" href="css/style.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="$1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
     <title>CMYK TO-DO LIST</title>
 </head>
@@ -22,7 +24,7 @@ include 'func.php';
         <div class="column"></div>
         <div class="column">
             <div class="has-text-centered">
-                <h1 class="title">Hi, <?php echo $_SESSION['userid'] ?>! </h1><br>
+                <h1 class="title is-capitalized">Hi, <?php echo $_SESSION['userid'] ?>! </h1><br>
             </div>
     <?php
 
@@ -51,66 +53,61 @@ include 'func.php';
               $id = $row['id'];
               $duedate =  $row['duedate'];
               $task =  $row['task'];?>
+                <tr>
+                    <td class="is-info"><b> <?php echo $task; ?> </b></td>
+                    <td class="is-primary"><b> <?php echo $duedate; ?> </b></td>
+                    <td>
+                        <a data-target="#edit<?php echo $id; ?>" data-toggle="modal"><button type='button' class='btn btn-warning btn-sm'> Edit</button></a>
+                    </td>
+                    <td>
+                        <a href='delete.php?id=<?php echo $row['id']; ?>'>
+                        <b>DONE</b></a>
+                    </td>
+                </tr>
 
 
-                    <tr>
-                        <td class="is-info"><b> <?php echo $task; ?> </b></td>
-                        <td class="is-primary"><b> <?php echo $duedate; ?> </b></td>
-                        <td>
-
-
-                            <button class="button modal-button" data-target="modal-ter" aria-haspopup="true">EDIT</button>
-                        </td>
-                        <td>
-                            <a href='delete.php?id=<?php echo $row['id']; ?>'>
-                            <b>DONE</b></button></a>
-                        </td>
-                    </tr>
-
-                      <!--edit modal -->
-                            <div id="modal-ter" class="modal">
-                                <div class="modal-background">
-                                    <div class="modal-card" style="margin-top: 250px;">
-                                        <section class="modal-card-body">
-                                            <div class="content">
-
-                                                <form method="POST" action="editTask.php">
-                                                    <div class="columns">
-                                                        <div class="column">
-                                                            <label class="font1"><b>EDIT TASK</b></label>
-                                                            <input type="hidden" name="id" value="<?php echo $id; ?>">
-                                                            <div class="control">
-                                                                <input class="input is-medium is-warning" style="width:10em;" type="text" name="task" value="<?php echo $task; ?>" placeholder="to-do..">
-                                                            </div>
-                                                        </div>
-                                                        <div class="column">
-                                                            <label class="font1"><b>DEADLINE</b></label>
-                                                            <div class="control">
-                                                                <input required class="input is-medium is-warning" style="width:10em;" type="date" name="date" value="<?php echo $duedate; ?>" placeholder="dd/mm/yyy">
-                                                            </div>
+                
+                    <div class="modal fade" id="edit<?php echo $id; ?>">
+                        <div class="modal-background">
+                            <div class="modal-card" style="margin-top: 200px;">
+                                <section class="modal-card-body">
+                                    <!-- Modal content-->
+                                    <div class="content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        </div>
+                                        <br>
+                                            <form method="POST" action="editTask.php">
+                                                <div class="columns">
+                                                    <div class="column">
+                                                        <input type="hidden" name="id" value="<?php echo $id; ?>">
+                                                        <label class="font1"><b>EDIT TASK</b></label>
+                                                        <div class="control">
+                                                            <input class="input is-large is-warning" style="width:15em;" type="text" name="task" value="<?php echo $task; ?>" placeholder="to-do..">
                                                         </div>
                                                     </div>
-                                            </div>
-                                        </section>
-                                                    <footer class="modal-card-foot">
-                                                        <button class="button is-danger font1" type="submit" name="submit"><b>UPDATE TASK</b></button>
-                                                        </form>
-                                                        <button class="button is-info font1"><b>CANCEL</b></button>
-                                                    </footer>
-
+                                                    <div class="column">
+                                                        <label class="font1"><b>DEADLINE</b></label>
+                                                        <div class="control">
+                                                            <input required class="input is-large is-warning" style="width:15em;" type="date" name="date" value="<?php echo $duedate; ?>" placeholder="dd/mm/yyy">
+                                                        </div>
+                                                    </div>
+                                                </div>
                                     </div>
-                                </div>
-                            </div>
-
+                                </section>
+                                    <footer class="modal-card-foot">
+                                        <button class="button is-danger font1 is-large" type="submit" name="submit"><b>UPDATE</b></button>
+                                            </form>
+                                    </footer>
+                        </div>
+                    </div>
+                </div>
             <?php endwhile; ?>
-
             </tbody>
             </table>
     <?php else : ?>
-        <h1 class="is-white font1"><b>No task as of the moment.</b></h1><br>
+        <h1 class="is-white font1" style="font-size: 25px; color: white;"><b>No task as of the moment.</b></h1><br>
     <?php endif;
-
-
     $conn->close(); ?>
 
         <section id="home" class="hero is-warning is-centered">
@@ -120,19 +117,19 @@ include 'func.php';
                         <div class="column">
                             <label class="font1"><b>NEW TASK</b></label>
                             <div class="control">
-                                <input class="input is-medium" style="width:15em;" type="text" name="task" placeholder="to-do.." required>
+                                <input class="input is-large" style="width:15em;" type="text" name="task" placeholder="to-do.." required>
                             </div>
                         </div>
 
                         <div class="column">
                             <label class="font1"><b>DEADLINE</b></label>
                             <div class="control">
-                                <input class="input is-medium" style="width:15em;" type="date" name="date" placeholder="dd/mm/yyy" required>
+                                <input class="input is-large" style="width:15em;" type="date" name="date" placeholder="dd/mm/yyy" required>
                             </div>
                         </div>
                     </div>
                     <div class="control">
-                        <button class="button is-danger font1"type="submit" name="insert"><b>ADD</b></button>
+                        <button class="button is-danger font1 is-large"type="submit" name="insert"><b>ADD</b></button>
                     </div>
                 </form>
             </div>
@@ -146,9 +143,6 @@ include 'func.php';
         <div class="column"></div>
     </div>
 </div>
-
-
-
 
 
 <script>
